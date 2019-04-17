ddc76a3af1c3bd0768db6ee6adc6cc3b62b9d3da
syz <4689902@qq.com>
2019-04-17 20:27:33 +0800

    init

---
+++ assets/resources/map/tiles.tsx
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="50" tileheight="50" tilecount="5">
+ <image source="tiles.png" width="50" height="250"/>
+ <terraintypes>
+  <terrain name="新地形" tile="-1"/>
+ </terraintypes>
+</tileset>
