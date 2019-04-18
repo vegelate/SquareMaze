e1d7bf47c14da812123565c64eb3035681cfcc95
syz <4689902@qq.com>
2019-04-18 20:00:16 +0800

    出生角色

--- assets/Script/GamePlay.js
+++ assets/Script/GamePlay.js
@@ -7,9 +7,13 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+var Hero = require("Hero")
 
-cc.Class({
+var GamePlay = cc.Class({
     extends: cc.Component,
+    statics:{
+        instance: null
+    },
 
     properties: {
         // foo: {
@@ -28,27 +32,31 @@ cc.Class({
         //     }
         // },
 
+
+
         map:{
             default: null,
             type: cc.TiledMap,
-        },
-
-        // 下墙
-        wallBottomLayer:{
-            default:null,
-            type: cc.TiledLayer,
-        },
-
-        // 上墙
-        wallUpLayer:{
-            default:null,
-            type: cc.TiledLayer, 
         },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        GamePlay.instance = this
+
+        let self = this
+        this.node.on(cc.Node.EventType.TOUCH_START, function ( event ) {
+            console.log('Touch Start');
+            self.onTouchStart(event.getLocationX(), event.getLocationY())
+          });
+
+        this.node.on(cc.Node.EventType.TOUCH_END, function ( event ) {
+            console.log('Touch End');
+            self.onTouchEnd(event.getLocationX(), event.getLocationY())
+          });
+
+    },
 
     start () {
         let self = this
@@ -65,6 +73,7 @@ cc.Class({
             
             self.wallUpLayer = self.map.getLayer("WallUp")
             self.wallBottomLayer = self.map.getLayer("WallBottom")
+            self.playLayer = self.map.getLayer("Play")
 
             var objects = self.map.getObjectGroup("Object")
             var heroObj = objects.getObject("Hero")
@@ -72,27 +81,84 @@ cc.Class({
             var m2Obj = objects.getObject("M2")
             var m3Obj = objects.getObject("M3")
 
-            var heroPos = cc.v2(heroObj.x, heroObj.y)
-
-            var heroCoord = self.getTileCoord(heroPos)
+            let heroPos = cc.v2(heroObj.x, heroObj.y)
+            let heroCoord = self.posToCoord(heroPos)
 
             cc.log("hero pos:", heroPos.toString())
             cc.log("hero coord:", heroCoord.toString())
             
+            self.spawnHero("prefab/hero", heroCoord)
         })
     },
+
+    // 出生英雄 
+    spawnHero(path, coord){
+        let self = this;
+        cc.loader.loadRes(path, function (err, prefab) {
+            var newNode = cc.instantiate(prefab);
+            self.playLayer.node.addChild(newNode);
+
+            self.hero = newNode.getComponent(Hero)
+            self.hero.setCoord(coord)
+        });
+    },
+
+    // 触屏开始
+    onTouchStart(x, y){
+        this.touchStartPos = cc.v2(x, y)
+        cc.log("touch start pos:", this.touchStartPos.toString())
+    },
+
+    // 触屏结束
+    onTouchEnd(x, y){
+        if (this.touchStartPos == null) {
+            cc.log("touchStartPos is null")
+            return
+        }
+
+        let touchEndPos = cc.v2(x, y)
+
+        let d = touchEndPos.sub(this.touchStartPos);
+        if (Math.abs(d.x) > Math.abs(d.y)){
+            // x 移动
+            if (d.x > 0){
+                cc.log("Move to x+")
+            }else if (d.x < 0){
+                cc.log("Move to x-")
+            }
+        }else if (Math.abs(d.y) > Math.abs(d.x)){
+            // y 移动
+            if (d.y > 0){
+                cc.log("Move to y+")
+            }else if (d.y < 0){
+                cc.log("Move to y-")
+            }
+        }
+
+    },
+
     // update (dt) {},
 
     // 地图像素坐标转成瓦片单位坐标
-    getTileCoord(pos){
+    posToCoord(pos){
         var mapSize = this.node.getContentSize();
         var tileSize = this.map.getTileSize();
 
-        cc.log("map size:", mapSize.toString())
-        cc.log("tile size:", tileSize.toString())
+        //cc.log("map size:", mapSize.toString())
+        //cc.log("tile size:", tileSize.toString())
 
         var x = Math.floor(pos.x / tileSize.width);
         var y = Math.floor((mapSize.height - pos.y) / tileSize.height);
+
+        return cc.v2(x, y);
+    },
+
+    coordToPos(coord){
+        var mapSize = this.node.getContentSize();
+        var tileSize = this.map.getTileSize();
+
+        var x = (coord.x+0.5) * tileSize.width;
+        var y = mapSize.height - (coord.y+0.5) * tileSize.height;
 
         return cc.v2(x, y);
     },
